c71c420ce6b03cd525c6d4b15af6c6a680ba1232
loyanix <loyanmaksim@gmail.com>
2022-10-07 17:29:58 +0300

    Initial commit

---
+++ src/main/java/com/loyanix/sosialnetwork/SosialNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.loyanix.sosialnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SosialNetworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SosialNetworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/loyanix/sosialnetwork/SosialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loyanix.sosialnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SosialNetworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
